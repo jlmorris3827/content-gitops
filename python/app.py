6e9a64bb6b9ee6399bfb206f94cd589ee6a58a8c
Jim <jim.morris@mobiledgex.com>
2022-03-27 11:35:40 -0500

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "testing on commit"
+    return "testing on commit2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
